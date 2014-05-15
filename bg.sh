3785b02edfc175487af7caac1c43d6df74de4fa9
Derek <derek@defiant>
2014-05-15 17:07:47 -0500

    Man i'm bad at git

--- bg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while true;
-do
-    rm -f bg.txt
-    ls ~/images/wallpapers | sort -R | tail -n 1 > ~/images/wallpapers/bg.txt
-    nitrogen --set-auto ~/images/wallpapers/`cat ~/images/wallpapers/bg.txt`
-    sleep 5m
-done
